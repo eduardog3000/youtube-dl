c6d34599a9631d529dd1af6e1b871f3389ae339e
TheDaChicken <steve.rock.pet@gmail.com>
2018-12-12 19:43:43 -0500

    [CartoonNetwork] Fixed AttributeError
    * [CartoonNetwork] Changed cvp to ngtv
    
     Changed cvp to ngtv and also fixed AttributeError: 'NoneType' object has no attribute 'groups'
    
    * Just removed some code that is not needed
    
    * Removed none needed stuff
    
    * Fixed cartoonnetwork not working with Adobe Pass
    
    * [CartoonNetwork] Made it in the format of everything else.
    
    * [CartoonNetwork] Changes stuff I forgot
    
    Changed description's label from authType to description
    
    * [CartoonNetwork] Fixed shortclips
    
    ShortClips don't have descriptions, so it will crash but now it's fixed.
    
    * [Cartoonnetwork] Allowing more support if the website changes.
    
    * Fixed Adobe Pass thinking it's needs TV logins
    
    * Now lets see if that fixed adobe pass.
    
    * my my let's try again.
    
    * I think I got it now lol
    
    * Update cartoonnetwork.py
    
    * Update cartoonnetwork.py
    
    * Update cartoonnetwork.py
    
    * [CartoonNetwork] Cleaned up some of the code.
    
    * Cleaned up all of the code.
    
    Changed auth around so you don't need to use if statements and more. :/
    
    * Removed something I forgot to remove lol
    
    * Changed something since title was something else.

--- youtube_dl/extractor/cartoonnetwork.py
+++ youtube_dl/extractor/cartoonnetwork.py
@@ -9,13 +9,13 @@ from .turner import TurnerBaseIE
 class CartoonNetworkIE(TurnerBaseIE):
     _VALID_URL = r'https?://(?:www\.)?cartoonnetwork\.com/video/(?:[^/]+/)+(?P<id>[^/?#]+)-(?:clip|episode)\.html'
     _TEST = {
-        'url': 'http://www.cartoonnetwork.com/video/teen-titans-go/starfire-the-cat-lady-clip.html',
+        'url': 'https://www.cartoonnetwork.com/video/steven-universe/garnet-gets-a-job-clip.html',
         'info_dict': {
-            'id': '8a250ab04ed07e6c014ef3f1e2f9016c',
+            'id': '96bc363957a17bc03d16feb86b8391a321f7e670',
             'ext': 'mp4',
-            'title': 'Starfire the Cat Lady',
-            'description': 'Robin decides to become a cat so that Starfire will finally love him.',
-        },
+            'title': 'Garnet Gets a Job',
+            'description': 'Garnet and Steven explore the most unlikely timelines in Beach City.',
+		},
         'params': {
             # m3u8 download
             'skip_download': True,
@@ -25,18 +25,23 @@ class CartoonNetworkIE(TurnerBaseIE):
     def _real_extract(self, url):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
-        id_type, video_id = re.search(r"_cnglobal\.cvp(Video|Title)Id\s*=\s*'([^']+)';", webpage).groups()
-        query = ('id' if id_type == 'Video' else 'titleId') + '=' + video_id
-        return self._extract_cvp_info(
-            'http://www.cartoonnetwork.com/video-seo-svc/episodeservices/getCvpPlaylist?networkName=CN2&' + query, video_id, {
-                'secure': {
-                    'media_src': 'http://androidhls-secure.cdn.turner.com/toon/big',
-                    'tokenizer_src': 'https://token.vgtf.net/token/token_mobile',
-                },
-            }, {
+        video_id = self._html_search_regex(r'[^>]+.mediaId = "(.+?)"', webpage, 'video_id')	
+        title = self._html_search_regex(r'[^>]+.episodeTitle = "(.+?)"', webpage, 'title')
+        description = self._html_search_regex(r'[^>]+description[^>]*>(.+?)<', webpage, 'description', default=None)
+        info = self._extract_ngtv_info(
+            video_id,
+            {'networkId': 'cartoonnetwork'},
+            {
                 'url': url,
                 'site_name': 'CartoonNetwork',
                 'auth_required': self._search_regex(
-                    r'_cnglobal\.cvpFullOrPreviewAuth\s*=\s*(true|false);',
-                    webpage, 'auth required', default='false') == 'true',
-            })
+                    r'[^>]+currentVideo.authType = "(auth|unauth)"',
+                    webpage, 'auth required', default='false') == 'auth',
+            },
+        )
+        info.update({
+            'id': video_id,
+            'title': title,
+            'description': description,
+        })
+        return info
